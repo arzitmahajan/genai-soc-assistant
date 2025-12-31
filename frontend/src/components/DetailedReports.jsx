57b6edc17260453788f149b1718332363fe06f1c
Arzit <nidhimahajan251@gmail.com>
2026-01-01 04:19:19 +0530

    fix(frontend): minor UI alignment issue

--- frontend/src/components/DetailedReports.jsx
+++ frontend/src/components/DetailedReports.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 const DetailedReports = () => {
-  const {detailedReports} = useSelector((state)=>state.dashboard);
+  const { detailedReports } = useSelector((state) => state.dashboard);
   // const reports = [
   //   { id: 1, name: "Report Bam", date: "May 1, 2024", severity: "High/Low", category: "Malware" },
   //   { id: 2, name: "Report Bam", date: "May 1, 2024", severity: "Medium", category: "Phishing" },
@@ -11,29 +11,33 @@ const DetailedReports = () => {
   return (
     <div className="detailed-reports card">
       <h3>Detailed Reports</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Report</th>
-            <th>Upload Date</th>
-            <th>Severity Distribution</th>
-            <th>Threat Categories</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {detailedReports.map((r,idx) => (
-            <tr key={idx}>
-              <td>{r.name}</td>
-              <td>{r.date}</td>
-              <td>{r.severity}</td>
-              <td>{r.category}</td>
-              <td><a href="/">View</a></td>
+
+      <div className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Report</th>
+              <th>Upload Date</th>
+              <th>Severity Distribution</th>
+              <th>Threat Categories</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {detailedReports.map((r, idx) => (
+              <tr key={idx}>
+                <td>{r.name}</td>
+                <td>{r.date}</td>
+                <td>{r.severity}</td>
+                <td>{r.category}</td>
+                <td><a href="/">View</a></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
+
   );
 };
 
